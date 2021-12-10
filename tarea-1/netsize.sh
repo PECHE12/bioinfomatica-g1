ec76dcdc5cdfa207cf79e6f8b8a6244b2cb9fe88
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-10 13:32:51 -0500

    Update netsize.sh
    Cómo debes pensar antes de aprender a programar

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -1,1 +1,10 @@
+#  TAREA -1- BIOINFORMÁTICA   :shipit:
+
+Tutorial de conceptos básicos  para aprender el uso de  Python.
+
+##Cómo debes pensar antes de aprender a programar en Python.
+- A veces es difícil encontrar motivación, especialmente cuando nos enfrentamos a obstáculos que nos cambian la vida.
+- Los pensamientos son muy poderosos  y pueden  evitar que cumplas las  metas propuestas.
+- Puedo hacer lo que sea. Es una frase muy simple, pero te ayuda a recordar que realmente tienes la libertad de lograr lo que te propongas.
+- Nunca es tarde. Sin importar que tan “viejo” creas estar, siempre se puede hacer un alto y cambiar de dirección.
 
