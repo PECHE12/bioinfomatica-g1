18f004e18906726814c1a6c918744d4f07da01ac
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-10 15:39:18 -0500

    Update netsize.sh
    Cómo instalar Python de manera facil

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -16,3 +16,13 @@ Tutorial de conceptos básicos  para aprender el uso de  Python.
  
 ##  Imagen
 ![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
+## Instalar Python es facil.
+%   Ir al área de descargas para Windows del sitio oficial y descargar el instalador ejecutable Python 3.6 de 32 o 64 bit.
+%  Iniciar la instalación ejecutando el archivo descargado Python-3.6.0.exe o Python-3.6.0-amd64.exe con doble clic. Si es necesario, confirmar la ejecución en la ventana de advertencia de seguridad de Abrir archivo.
+%   Una vez iniciado el instalador, en la ventana Install Python 3.6.0 (64 bit) activar las casillas de las opciones: Install launcher for all users (recommended) y Add Python 3.6 to PATH. Después, continuar seleccionando la opción Customize installation. Choose Location and features.
+%   En la ventana Optional features verificar que están activas todas las opciones y hacer clic en el botón [Next].
+%  En la ventana Advanced Options verificar que están activas las opciones de la imagen, escribir la ruta del directorio de instalación "C:\Python36" (o aceptar la ruta por defecto) y comenzar la instalación haciendo clic en el botón [Install].
+%   A continuación, después de unos segundos de espera, comenzará el proceso de instalación de Python.
+%   En la ventana Setup was successful, una vez que ha concluido el proceso de instalación hacer clic en el botón [Close]. Desde esta ventana es posible acceder a un tutorial online de Python, a la documentación oficial del lenguaje y a información con las novedades de la presente versión.
+
