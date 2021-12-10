041aaf0007c294ffd6844ed6ef3287c416561048
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-10 13:56:25 -0500

    Update netsize.sh
    conceptos básicos para aprender programación

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -8,3 +8,9 @@ Tutorial de conceptos básicos  para aprender el uso de  Python.
 - Puedo hacer lo que sea. Es una frase muy simple, pero te ayuda a recordar que realmente tienes la libertad de lograr lo que te propongas.
 - Nunca es tarde. Sin importar que tan “viejo” creas estar, siempre se puede hacer un alto y cambiar de dirección.
 
+## Conceptos básicos para aprender a programar Python.
+*  Manipulación de cadenas.
+*  Manipulación de números.
+*  Manipulado de fecha y hora.
+*  Tratar con el sistema de archivos.
+ 
