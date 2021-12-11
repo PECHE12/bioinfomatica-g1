4afe74c47635d2f2b772a070e34a4d059720bac5
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 18:49:00 -0500

    Update netsizeall.sh
    resultados

--- tarea-1/netsizeall.sh
+++ tarea-1/netsizeall.sh
@@ -9,3 +9,5 @@ Este parte fue especificada en README.md
   Se utilizo for,do,done
   for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l;
   
+  # Para finalizar esta tarea se obtiene los resultados en la parte de netsize_all.txt con archivos de n59.txt
+
