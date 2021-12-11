bf90ae344aeb4edaf87dbeca3d7eb0dd0c30c897
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 17:10:00 -0500

    Update netsize.sh
    Se imprime contenidos de saavedra2013

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -2,7 +2,14 @@
 1.- Se requiere la ubicación en el Git bash, de preferencia en el programa de fácil uso.
 2.- Se requiere ubicar en la ruta:
    C:\Users\Usuario\Desktop\sexto 6\CSB\unix\data\Saavedra2013
- 
+# Se escribe en el terminal 
+cat n1.txt
+
+# Se imprime todo el contenido de Saavedra2013
+   echo "El número de columnas es:" ; head -n1.txt| grep -o"" | wc -1 ; echo"El número de filas es:" ; wc -1 n1.txt; echo "Fin de la ejecución"
+
+
+-
      
 
 
