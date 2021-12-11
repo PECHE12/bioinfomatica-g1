68e8b8f7e6861f5710e5da51e08580cf5836ccee
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 18:08:06 -0500

    Update netsize.sh
    Modificación de la ubicación de  ruta

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -1,7 +1,9 @@
 # PARA PODER REALIZAR EL EJERCICIO
 1.- Se requiere la ubicación en el Git bash, de preferencia en el programa de fácil uso.
 2.- Se requiere ubicar en la ruta:
-   C:\Users\Usuario\Desktop\sexto 6\CSB\unix\data\Saavedra2013
+     cd  Documents/CSB/unix
+## Se ubica el directorio y la ruta.
+
 # Se escribe en el terminal 
 cat n1.txt
 
