8466b43212bdec9240cfdf5bfb4d309e48718cb9
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 18:45:19 -0500

    Update netsizeall.sh
    comandos utilizados

--- tarea-1/netsizeall.sh
+++ tarea-1/netsizeall.sh
@@ -1,2 +1,11 @@
 #  Ejercicio-parte2  [Advanced]
 Este parte fue especificada en README.md
+-  Se elige la misma ruta:
+   cd  Documents/CSB/unix
+- Se corre la información 
+  cd Saavedra2013
+  
+  #Se ejecuta el código en git bash proporcionados de la clase
+  Se utilizo for,do,done
+  for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l;
+  
