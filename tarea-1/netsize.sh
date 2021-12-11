f7d29c5eb8a9d5baf30da033a2c43f70be007ebf
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 14:29:28 -0500

    Update netsize.sh
    corrección de la tarea

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -1,36 +1,3 @@
 #  TAREA -1- BIOINFORMÁTICA   :shipit:
 
-Tutorial de conceptos básicos  para aprender el uso de  Python.
 
-##Cómo debes pensar antes de aprender a programar en Python.
-- A veces es difícil encontrar motivación, especialmente cuando nos enfrentamos a obstáculos que nos cambian la vida.
-- Los pensamientos son muy poderosos  y pueden  evitar que cumplas las  metas propuestas.
-- Puedo hacer lo que sea. Es una frase muy simple, pero te ayuda a recordar que realmente tienes la libertad de lograr lo que te propongas.
-- Nunca es tarde. Sin importar que tan “viejo” creas estar, siempre se puede hacer un alto y cambiar de dirección.
-
-## Conceptos básicos para aprender a programar Python.
-*  Manipulación de cadenas.
-*  Manipulación de números.
-*  Manipulado de fecha y hora.
-*  Tratar con el sistema de archivos.
- 
-##  Imagen
-![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
-
-## Instalar Python es facil.
-%   Ir al área de descargas para Windows del sitio oficial y descargar el instalador ejecutable Python 3.6 de 32 o 64 bit.
-%  Iniciar la instalación ejecutando el archivo descargado Python-3.6.0.exe o Python-3.6.0-amd64.exe con doble clic. Si es necesario, confirmar la ejecución en la ventana de advertencia de seguridad de Abrir archivo.
-%   Una vez iniciado el instalador, en la ventana Install Python 3.6.0 (64 bit) activar las casillas de las opciones: Install launcher for all users (recommended) y Add Python 3.6 to PATH. Después, continuar seleccionando la opción Customize installation. Choose Location and features.
-%   En la ventana Optional features verificar que están activas todas las opciones y hacer clic en el botón [Next].
-%  En la ventana Advanced Options verificar que están activas las opciones de la imagen, escribir la ruta del directorio de instalación "C:\Python36" (o aceptar la ruta por defecto) y comenzar la instalación haciendo clic en el botón [Install].
-%   A continuación, después de unos segundos de espera, comenzará el proceso de instalación de Python.
-%   En la ventana Setup was successful, una vez que ha concluido el proceso de instalación hacer clic en el botón [Close]. Desde esta ventana es posible acceder a un tutorial online de Python, a la documentación oficial del lenguaje y a información con las novedades de la presente versión.
-
-:bamboo: :santa: :gift: :full_moon_with_face:  :panda_face:
-
-## Mini-tutorial de introducción a Python.
-Aqui esta el link<https://www.youtube.com/watch?v=DAdRO6ByBoU> para personas que quieran empezar desde cero.
-
-Espero que te  haya gustado  :heart_eyes: 
-**  El hombre que es un maestro de la paciencia es un maestro de todo lo demás**
-:bug:  
