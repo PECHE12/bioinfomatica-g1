b45928182193083a1d6775eb2668e8e92302dcc9
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 17:11:43 -0500

    Update netsize.sh
    Número de columnas

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -8,8 +8,11 @@ cat n1.txt
 # Se imprime todo el contenido de Saavedra2013
    echo "El número de columnas es:" ; head -n1.txt| grep -o"" | wc -1 ; echo"El número de filas es:" ; wc -1 n1.txt; echo "Fin de la ejecución"
 
+#RESULTADOS FINALES
+-Número de columnas 80
 
--
+
+
      
 
 
