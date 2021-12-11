8813dad7b7e0b9f76db4aa649a6bebcd6c3d67b4
Betsabe Arcos <94935700+PECHE12@users.noreply.github.com>
2021-12-11 16:56:02 -0500

    Update netsize.sh
    Lugar donde esta guardado el archivo saavedra 2013

--- tarea-1/netsize.sh
+++ tarea-1/netsize.sh
@@ -1,3 +1,8 @@
-#  TAREA -1- BIOINFORMÁTICA   :shipit:
+# PARA PODER REALIZAR EL EJERCICIO
+1.- Se requiere la ubicación en el Git bash, de preferencia en el programa de fácil uso.
+2.- Se requiere ubicar en la ruta:
+   C:\Users\Usuario\Desktop\sexto 6\CSB\unix\data\Saavedra2013
+ 
+     
 
 
